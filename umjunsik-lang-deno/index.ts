4b5e94f865e36bc4adc6b22d3c3ee86ec8d986fe
rycont <reactdev@kakao.com>
2020-08-04 20:00:33 +0900

    Umjunsik-lang_V2

--- umjunsik-lang-deno/index.ts
+++ umjunsik-lang-deno/index.ts
@@ -3,7 +3,8 @@ import { readLines } from "https://deno.land/std@v0.51.0/io/bufio.ts";
 
 export const run = async (content: string) => {
   let proceed = content.split('\n')
-  if(proceed[0] === '어떻게' && proceed.slice(-1)[0] === '이 사람이름이냐!') {
+  const encoder = new TextEncoder()
+  if(!(proceed[0] === '어떻게' && proceed.slice(-1)[0] === '이 사람이름이냐!')) {
     console.log("Error: 어떻게 이 코드가 엄랭이냐ㅋㅋ")
     return
   }
@@ -21,24 +22,45 @@ export const run = async (content: string) => {
     }
     if(a.includes('어')) numbered += variables[a.split('어').length - 1]
     if(a.includes('.')) numbered += a.split('.').length - 1
-    if(a.includes(',')) numbered += a.split(',').length - 1
+    if(a.includes(',')) numbered -= a.split(',').length - 1
     return numbered
   }
-  while(proceed[pointer] !== '이 사람이름이냐!') {
-    const operation = proceed[pointer++]
-    if(operation.includes('엄')) { // 변수대입
+  const stringify = (unicode: number) => {
+    return String.fromCharCode(unicode)
+  }
+
+  const parseOperation = async (operation: string): Promise<number|undefined> => {
+    if (operation.includes('동탄') && operation.includes('?')) { //IFGOTO
+      const condition = await numberify(operation.substring(2, operation.lastIndexOf('?') + 1))
+      if (condition === 0) {
+        return parseOperation(operation.substr(operation.lastIndexOf('?') + 1))
+      }
+      return
+    }
+    if(operation.includes('엄')) {
       const variablePointer = operation.split('엄')[0].split('어').length
       const setteeValue = await numberify(operation.split('엄')[1])
       variables[variablePointer] = setteeValue
-      continue
     }
-    if(operation.includes('식') && operation.includes('!')) { // 콘솔출력
-      console.log(await numberify(operation.slice(1, -1)))
+    if (operation.includes('식') && operation[operation.length - 1] === '!') {
+      await Deno.stdout.write(encoder.encode(String(await numberify(operation.slice(1, -1)))))
     }
-    if(operation.includes('준')) { //GOTO
+    if (operation.includes('식') && operation[operation.length - 1] === 'ㅋ') {
+      if(operation === '식ㅋ') console.log()
+      await Deno.stdout.write(encoder.encode(stringify(await numberify(operation.slice(1, -1)))))
+    }
+    if(operation.includes('준')) {
       pointer = await numberify(operation.split('준')[1]) - 1
     }
-    if(operation === '화이팅!') return numberify(operation.split('화이팅!')[1])
+    if (operation.indexOf('화이팅!') === 0) {
+      return numberify(operation.split('화이팅!')[1])
+    }
+  }
+  
+  while(proceed[pointer] !== '이 사람이름이냐!') {
+    const operation = proceed[pointer++]
+    const evaludated = await parseOperation(operation)
+    if(evaludated) return evaludated
   }
 }
 
