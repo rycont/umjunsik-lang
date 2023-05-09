1f1b5c032645f7a491e68dd74a2dcb4d75265362
Hiyabye <hiyabye0465@gmail.com>
2023-05-09 13:20:27 +0900

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     entry_points={
         "console_scripts": [
-            "umjunsik-lang-python=umjunsik-lang-python.__main__:main",
+            "umjunsik=umjunsik-lang-python.__main__:main",
         ],
     },
     author="rycont",
